4aa96a607dd87f50d4f7877e4dc3832e114ffbd8
Javier Luraschi <javierluraschi@hotmail.com>
2016-02-10 09:14:13 -0800

    fix s3 sample mp4 paths

--- src/channel.build.web/channel.build.web.js
+++ src/channel.build.web/channel.build.web.js
@@ -34,16 +34,14 @@ if (Meteor.isServer) {
   });
 
   Picker.route("/api/channels/:_id/videos/", function(params, req, res) {
-    var base = "https://s3.amazonaws.com/channel.build.oregon/gwces2016/"
+    var base = "https://s3-us-west-2.amazonaws.com/channel.build.oregon/gwces2016/"
     res.end(JSON.stringify({
       videos: [
         base + "Team+GeekWire+arrives+at+CES+2016-woNBwkaL18M.mp4",
         base + "CES+2016+-+Day+1+recap-opjNvc38Xcc.mp4",
         base + "CES+2016+Day+2+recap-6eFy9oYSj94.mp4",
         base + "Jai+Ho+performance+at+Intel+press+conference+CES+2016-bhFUe78bnoQ.mp4",
-        base + "CES+2016+-+Day+3+recap-QJ9-4bfvJ0M.mp4",
         base + "Microsoft's+epic+CES+party+in+Las+Vegas+with+Steve+Aoki-FAjpXtzB4KA.mp4",
-        base + "CES+2016+-+Day+4+recap-JVulaJ1tqOk.mp4",
         base + "Shaquille+O'Neal+interview+at+CES+2016-D8h4LcSk7R.mp4",
         base + "Kenny+Smith+interview+at+CES+2016-S7WFb-jCzUk.mp4",
         base + "Charles+Barkley+interview+at+CES+2016-IEQnpny9kIE.mp4",
