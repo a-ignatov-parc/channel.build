bc14a7a6a98304e1d9dc05c7efb71aa33fb90bf2
Javier Luraschi <javierluraschi@hotmail.com>
2016-02-09 23:03:21 -0800

    add static api for videos under /api/channels/1/videos/

--- src/channel.build.web/channel.build.web.js
+++ src/channel.build.web/channel.build.web.js
@@ -32,4 +32,23 @@ if (Meteor.isServer) {
     clientId: "578510381054-1ihsp36ih3fmfrtg629pcok47fa9nh4d.apps.googleusercontent.com",
     secret: "N5P4tjO5Z2akK_c1XlCZOU2X"
   });
+
+  Picker.route("/api/channels/:_id/videos/", function(params, req, res) {
+    var base = "https://s3.amazonaws.com/channel.build.oregon/gwces2016/"
+    res.end(JSON.stringify({
+      videos: [
+        base + "Team+GeekWire+arrives+at+CES+2016-woNBwkaL18M.mp4",
+        base + "CES+2016+-+Day+1+recap-opjNvc38Xcc.mp4",
+        base + "CES+2016+Day+2+recap-6eFy9oYSj94.mp4",
+        base + "Jai+Ho+performance+at+Intel+press+conference+CES+2016-bhFUe78bnoQ.mp4",
+        base + "CES+2016+-+Day+3+recap-QJ9-4bfvJ0M.mp4",
+        base + "Microsoft's+epic+CES+party+in+Las+Vegas+with+Steve+Aoki-FAjpXtzB4KA.mp4",
+        base + "CES+2016+-+Day+4+recap-JVulaJ1tqOk.mp4",
+        base + "Shaquille+O'Neal+interview+at+CES+2016-D8h4LcSk7R.mp4",
+        base + "Kenny+Smith+interview+at+CES+2016-S7WFb-jCzUk.mp4",
+        base + "Charles+Barkley+interview+at+CES+2016-IEQnpny9kIE.mp4",
+        base + "Kia+VR+experience+in+self-driving+Soul+EV+hatchback-GewwoTOjjdE.mp4"
+      ]
+    }));
+  });
 }
