a573480ccf007dc5c02f03b8858d5f163fb7d55d
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-05 00:17:06 +0300

    fix carousel bug, implement fuzzy search for videos by title

--- src/channel.build.tvos/client/js/presenter.js
+++ src/channel.build.tvos/client/js/presenter.js
@@ -19,33 +19,69 @@ class Presenter {
   onSelect(event) {
     const target = event.target,
           template = target.getAttribute('template'),
+          onrender = target.getAttribute('onrender'),
           data = target.getAttribute('data'),
           dataMethod = target.getAttribute('data-method'),
           dataArgs = target.getAttribute('data-args'),
           presentMethod = target.getAttribute('present-method'),
           dataPromise = data ? this.dataController.getData(data) :
                                dataMethod ? this.dataController.getDataFromMethod(dataMethod, dataArgs) :
-                                            Promise.resolve({});
+                                            Promise.resolve({}),
+          onRenderHandlers = (onrender ? onrender.split(';') : [])
+                             .concat(template ? ['setSelectEventHandler'] : [])
+                             .map((handler) => this[handler] ? this[handler].bind(this) : null)
+                             .filter((handler) => handler !== null);
 
     console.log(`Selecting ${target} with template="${template}", data="${data}",
                  data-method="${dataMethod}, data-args="${dataArgs}", present-method="${presentMethod}"...`);
 
     if (this[presentMethod]) {
+      const present = this[presentMethod].bind(this);
       console.log(`Requesting data for ${target}...`);
       dataPromise.then((data) => {
         console.log(`Received data for ${target}: ${data}`);
         // Store target to access it in present methods.
         data.target = target;
-        this[presentMethod](template, data);
+
+        // If template name is present then load it and call present method.
+        // Otherwise simply run present method. Also call onrender handlers.
+        if (template) {
+          this.resourceLoader.getTvml(template, data).then((doc) => {
+            onRenderHandlers.forEach((handler) => handler({doc, data}));
+            present({doc, data});
+          });
+        } else {
+          onRenderHandlers.forEach((handler) => handler({data}));
+          present({data});
+        }
       });
     }
   }
 
-  presentDoc(name, data) {
-    return this.resourceLoader.getTvml(name, data).then((doc) => {
-      doc.addEventListener('select', this.onSelect);
-      navigationDocument.pushDocument(doc);
+  onVideoSearch({keyboard, partial, doc, data}) {
+    const query = keyboard.text,
+          resultsContainer = doc.getElementById('search-results'),
+          results = this.dataController.fuzzySearchVideos(query, data),
+          resultDocPromises = results.map((video) => this.resourceLoader.getTvml(partial, video));
+
+    Promise.all(resultDocPromises).then((resultDocs) => {
+      const resultNodes = resultDocs.map((rdoc) => rdoc.documentElement);
+
+      // Clear all the previous results.
+      while (resultsContainer.lastChild) {
+        resultsContainer.removeChild(resultsContainer.lastChild);
+      }
+
+      // Fill the new results.
+      resultNodes.forEach((rnode) => {
+        doc.adoptNode(rnode);
+        resultsContainer.appendChild(rnode);
+      });
     });
+  }
+
+  presentDoc({doc, data}) {
+    navigationDocument.pushDocument(doc);
   }
 
   /**
@@ -54,26 +90,20 @@ class Presenter {
    * @param  {object}  data Data for the template.
    * @return {promise}      Promise with empty result.
    */
-  presentModal(name, data) {
-    return this.resourceLoader.getTvml(name, data).then((doc) => {
-      doc.addEventListener('select', this.onSelect);
-      navigationDocument.presentModal(doc);
-    });
+  presentModal({doc, data}) {
+    navigationDocument.presentModal(doc);
   }
 
-  presentMenuBarItem(name, data) {
+  presentMenuBarItem({doc, data}) {
     const menuItem = data.target,
           feature = menuItem.parentNode.getFeature('MenuBarDocument');
 
-    return this.resourceLoader.getTvml(name, data).then((doc) => {
-      if (feature && !feature.getDocument(menuItem)) {
-        doc.addEventListener('select', this.onSelect);
-        feature.setDocument(doc, menuItem);
-      }
-    });
+    if (feature && !feature.getDocument(menuItem)) {
+      feature.setDocument(doc, menuItem);
+    }
   }
 
-  presentVideo(name, data) {
+  presentVideo({data}) {
     let player = new Player(),
         video = new MediaItem('video', data.url);
 
@@ -84,6 +114,17 @@ class Presenter {
     player.playlist.push(video);
 
     player.play();
+  }
+
+  setSelectEventHandler({doc}) {
+    doc.addEventListener('select', this.onSelect);
+  }
+
+  setVideoSearchEventHandler({doc, data}) {
+    const searchField = doc.getElementsByTagName('searchField').item(0),
+          partial = searchField.getAttribute('result-partial');
+    let keyboard = searchField.getFeature('Keyboard');
+    keyboard.onTextChange = this.onVideoSearch.bind(this, {keyboard, partial, doc, data});
   }
 }
 
