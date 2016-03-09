91ee24e069e0f19f65d054d720e3a5b7383dd5e7
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-09 20:01:13 +0400

    add setup for cli to prepare-machine script

---
+++ scripts/prepare-machine.sh
@@ -0,0 +1,15 @@
+# TODO:
+# - Setup Vagrant.
+# - Test the script with Vagrant.
+
+RUBY_VERSION=2.2.1
+SHELL=bash
+
+# Download the signature for RVM.
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+
+# Install RVM and Ruby.
+curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --ruby
+echo source ~/.rvm/scripts/rvm > ~/.${SHELL}rc
+rvm install ruby-${RUBY_VERSION}
+rvm use ruby-${RUBY_VERSION}
