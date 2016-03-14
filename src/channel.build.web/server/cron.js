514e97f85f8f2eac7dc8a28bfbf5322e7bb55b25
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-14 21:09:44 +0300

    setup cron tasks, add mongo backup task #16

---
+++ src/channel.build.web/server/cron.js
@@ -0,0 +1,25 @@
+SyncedCron.config({
+  utc: true
+});
+
+// NOTE: https://sheharyar.me/blog/regular-mongo-backups-using-cron/
+SyncedCron.add({
+  name: 'MongoDB backup',
+  schedule: function (parser) {
+    return parser.text('at 00:00');
+  },
+  job: function () {
+    var database = 'meteor',
+        appname = 'channel.build',
+        mongoUrl = process.env.MONGO_URL,
+        host = mongoUrl.match(/mongodb:\/\/(.*)\/meteor/)[1],
+        timestamp = shell.exec('date +%F-%H%M').output.trim(),
+        backupsPath = `${process.env.HOME}/backups/${appname}`,
+        archivePath = `${backupsPath}/${appname}-${timestamp}.gzip`;
+
+    shell.mkdir('-p', backupsPath);
+    shell.exec(`mongodump --host ${host} --db ${database} --gzip --archive=${archivePath}`);
+  }
+});
+
+SyncedCron.start();
