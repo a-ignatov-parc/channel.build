aeebdc48fc772d1e62f31f360e74e2612bfc6fd2
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-18 00:31:12 +0300

    implement background jobs worker as a separate nodejs app, setup deployment settings

---
+++ src/channel.build.worker/chan-worker.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  var Job = require('meteor-job'),
+      JobsWorker = require('./jobs-worker'),
+      Shell = require('shelljs');
+
+  var chanWorker = new JobsWorker(process.env.WORKER_HOST, process.env.WORKER_PORT);
+  chanWorker.connect(function () {
+    return Job.processJobs('chan', 'import', {
+        concurrency: 1
+      }, function (job, callback) {
+        var chan = process.env.CHAN_PATH,
+            channelId = job.data.channelId,
+            command = `${chan} import ${channelId}`;
+            res = Shell.exec(command);
+
+        job.log(`output: ${res.output}`);
+        if (res.code === 0) { job.done(); } else { job.fail(); }
+        callback();
+      });
+  });
+})();
---
+++ src/channel.build.worker/jobs-worker.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  var DDP = require('ddp'),
+      DDPLogin = require('ddp-login'),
+      Job = require('meteor-job');
+
+  var JobsWorker = function (host, port) {
+    // Setup the DDP connection.
+    var _account = process.env.WORKER_ACCOUNT,
+        _pass = process.env.WORKER_PASS,
+        _ddp = new DDP({
+          host: host,
+          port: port,
+          use_ejson: true
+        });
+
+    // Connect Job with this DDP session.
+    Job.setDDP(_ddp);
+
+    this.connect = function (startWorkers) {
+      startWorkers = typeof startWorkers === 'function' ? startWorkers : function () {};
+      // Open the DDP connection.
+      _ddp.connect(function (err) {
+        if (err) throw err;
+        // Login to Meteor app and start workers to process jobs queue.
+        DDPLogin(_ddp, {
+          account: _account,
+          pass: _pass
+        }, function (err, token) {
+          if (err) throw err;
+          console.log(`Logged in to ${host}:${port} as ${_account}. Starting workers...`);
+          startWorkers();
+        });
+      });
+    };
+  };
+
+  module.exports = JobsWorker;
+})();
