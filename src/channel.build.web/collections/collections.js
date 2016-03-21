4f52e1812395af2ab21e1259be0d248da7efe2ed
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-21 14:45:55 +0300

    setup analytics collection with timestamps #18

--- src/channel.build.web/collections/collections.js
+++ src/channel.build.web/collections/collections.js
@@ -1,6 +1,13 @@
 Apps = new Mongo.Collection('apps');
 Invites = new Mongo.Collection('invites');
 Videos = new Mongo.Collection('videos');
+Analytics = new Mongo.Collection('analytics');
 
 // Background jobs collections.
 ChanJobs = JobCollection('chan');
+
+// Configure timestamps fields for collections using 'zimme:collection-timestampable' package..
+Analytics.attachBehaviour('timestampable',{
+  createdBy: false,
+  updatedBy: false
+});
