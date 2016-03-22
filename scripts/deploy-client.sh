42e13b7b116ba726e2e6d207ee4c2c036d959865
Michael Kalygin <michael.kalygin@gmail.com>
2016-03-22 17:33:23 +0300

    fix deploy-client script so that static tvos files works on production

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -9,8 +9,9 @@ alias rsh='ssh -i $KEY_PATH $TARGET_HOST'
 alias rcpdir='scp -r -i $KEY_PATH'
 
 pushd $CLIENT_PATH > /dev/null
-rsh mkdir -p $TARGET_CLIENT_PATH
-rsh rm -r $TARGET_CLIENT_PATH
+rsh rm -rf $TARGET_CLIENT_PATH
+rsh mkdir -p $TARGET_CLIENT_PATH/dist
+rsh mkdir -p $TARGET_CLIENT_PATH/public/tvml
 rcpdir $CLIENT_PATH/dist $TARGET_HOST:$TARGET_CLIENT_PATH
-rcpdir $CLIENT_PATH/public/tvml $TARGET_HOST:$TARGET_CLIENT_PATH
+rcpdir $CLIENT_PATH/public/tvml $TARGET_HOST:$TARGET_CLIENT_PATH/public
 popd > /dev/null
