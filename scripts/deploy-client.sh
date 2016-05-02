94cbd5bb1aafd14a2d5c21a0cd773b2032cd0892
Michael Kalygin <michael.kalygin@gmail.com>
2016-05-02 21:12:15 +0300

    implement versioning support for tvos client code

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -1,14 +1,16 @@
+[[ -n $1 ]] && CLIENT_VERSION=v$1 || CLIENT_VERSION=v1
 REPO_ROOT_PATH=$(cd `dirname $0`/..; pwd -P)
 KEY_PATH=$REPO_ROOT_PATH/secrets/channelbuild.pem
 CLIENT_PATH=$REPO_ROOT_PATH/src/channel.build.tvui
 TARGET_HOST=ubuntu@channel.build
-TARGET_CLIENT_PATH=/home/ubuntu/app/channel.build.tvui
+TARGET_CLIENT_PATH=/home/ubuntu/app/channel.build.tvui/$CLIENT_VERSION
 
 # Remote shell command and remote copy command.
 alias rsh='ssh -i $KEY_PATH $TARGET_HOST'
 alias rcpdir='scp -r -i $KEY_PATH'
 
 pushd $CLIENT_PATH > /dev/null
+npm install
 rsh rm -rf $TARGET_CLIENT_PATH
 rsh mkdir -p $TARGET_CLIENT_PATH/dist
 rsh mkdir -p $TARGET_CLIENT_PATH/public/tvml
---
+++ src/channel.build.tvos/channel/HTTP.swift
@@ -0,0 +1,45 @@
+//
+//  HTTP.swift
+//  channel
+//
+//  Created by Michael Kalygin on 02/05/16.
+//  Copyright © 2016 caffeinelabs. All rights reserved.
+//
+
+import Foundation
+
+class HTTP {
+  static let instance = HTTP()
+  private init() {}
+  
+  typealias JSONResponseHandler = (AnyObject?, NSError?) -> Void
+  
+  enum HTTPMethod: String {
+    case GET
+  }
+  
+  class func get(url: NSURL, completion: JSONResponseHandler? = nil) {
+    let request = NSMutableURLRequest(URL: url)
+    let session = NSURLSession.sharedSession()
+
+    request.HTTPMethod = HTTPMethod.GET.rawValue
+    
+    let task = session.dataTaskWithRequest(request) { data, response, error in
+      guard let data = data, json = JSON.fromNSData(data) else {
+        completion?(nil, error)
+        return
+      }
+      
+      completion?(json, error)
+    }
+    
+    task.resume()
+  }
+  
+  class func get(url: String, completion: JSONResponseHandler? = nil) {
+    guard let url = NSURL(string: url) else {
+      fatalError("Unable to create NSURL!")
+    }
+    self.get(url, completion: completion)
+  }
+}
