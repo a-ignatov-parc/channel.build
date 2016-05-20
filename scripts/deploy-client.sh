082940df96614c3c7a6bb5d6fb0b8bf5f87e73b2
Michael Kalygin <michael.kalygin@gmail.com>
2016-05-20 15:41:53 +0300

    prepare /scripts for open-source, add comments, fix security issue with pem keys

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -1,14 +1,46 @@
+#!/bin/bash
+#=========================================================================================
+#          FILE:  deploy-client.sh
+#         USAGE:  ./deploy-client.sh [version]
+#
+#  REQUIREMENTS:  Target machine's identity file must be configured for SSH
+#                 either through ~/.ssh/config or ssh-add. You may also
+#                 edit this script and use `ssh -i /path/to/your/key $TARGET_HOST`.
+#
+#         NOTES:  Please use your own value for $TARGET_HOST variable.
+#                 You may also setup other variables for your needs.
+#
+#   DESCRIPTION:  This script deploys TVML/TVJS code to target machine
+#                 with Meteor app running. The Meteor app implements endpoints
+#                 to access TVML/TVJS files with versioning support:
+#
+#                 /tvos/v{version}/{type}/{filepath}
+#
+#                 Where:
+#                 {version}  - a version of the TVML/TVJS code which may be
+#                              provided by this script. The default is 1.
+#                 {type}     - either 'tvml' or 'tvjs'.
+#                 {filepath} - path to the file.
+#
+#                 E.g.:
+#                 /tvos/v1/tvml/root.tvml
+#                 /tvos/v2/tvjs/app.js
+#=========================================================================================
+
+# Get the version parameter. The default is 1.
 [[ -n $1 ]] && CLIENT_VERSION=v$1 || CLIENT_VERSION=v1
+
 REPO_ROOT_PATH=$(cd `dirname $0`/..; pwd -P)
-KEY_PATH=$REPO_ROOT_PATH/secrets/channelbuild.pem
 CLIENT_PATH=$REPO_ROOT_PATH/src/channel.build.tvui
 TARGET_HOST=ubuntu@channel.build
 TARGET_CLIENT_PATH=/home/ubuntu/app/channel.build.tvui/$CLIENT_VERSION
 
 # Remote shell command and remote copy command.
-alias rsh='ssh -i $KEY_PATH $TARGET_HOST'
-alias rcpdir='scp -r -i $KEY_PATH'
+alias rsh='ssh $TARGET_HOST'
+alias rcpdir='scp -r'
 
+# Create all necessary directories on the target machine,
+# copy the TVML/TVJS code and build it.
 pushd $CLIENT_PATH > /dev/null
 npm install
 rsh rm -rf $TARGET_CLIENT_PATH
--- scripts/deploy-worker.sh
+++ scripts/deploy-worker.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
+#=========================================================================================
+#          FILE:  deploy-worker.sh
+#         USAGE:  ./deploy-worker.sh
+#
+#  REQUIREMENTS:  pm2 (http://pm2.keymetrics.io/) must be installed on the local machine:
+#                 npm install pm2 -g
+#
+#                 pm2, node.js and npm must be installed on the target machine.
+#
+#                 Target machine's identity file must be configured for SSH
+#                 either through ~/.ssh/config or ssh-add. You may also
+#                 edit this script and use `ssh -i /path/to/your/key $TARGET_HOST`.
+#
+#         NOTES:  Please use your own value for $TARGET_HOST variable.
+#                 You may also setup other variables for your needs.
+#
+#   DESCRIPTION:  This script deploys background jobs worker app to target machine
+#                 using pm2. You can also use pm2 to manage the worker app on
+#                 the target machine. Please refer to pm2 docs for more information:
+#                 http://pm2.keymetrics.io/docs/usage/quick-start/
+#=========================================================================================
+
 REPO_ROOT_PATH=$(cd `dirname $0`/..; pwd -P)
-KEY_PATH=$REPO_ROOT_PATH/secrets/channelbuild.pem
 WORKER_PATH=$REPO_ROOT_PATH/src/channel.build.worker
 TARGET_HOST=ubuntu@jobs.channel.build
 TARGET_WORKER_PATH=/home/ubuntu/app/channel.build/source/src/channel.build.worker
@@ -7,8 +29,10 @@ ECOSYSTEM_NAME=chan-ecosystem.json
 ENVIRONMENT=production
 
 # Remote shell command.
-alias rsh='ssh -i $KEY_PATH $TARGET_HOST'
+alias rsh='ssh $TARGET_HOST'
 
+# Deploy the worker app code to the target machine with pm2.
+# Use `pm2 deploy` only if this is the first deployment.
 pushd $WORKER_PATH > /dev/null
 rsh test -d $TARGET_WORKER_PATH || pm2 deploy $ECOSYSTEM_NAME $ENVIRONMENT setup
 pm2 deploy $ECOSYSTEM_NAME $ENVIRONMENT
