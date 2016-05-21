30cb8a61a3e775c9718ab5fcb91e8a79751032bd
Michael Kalygin <michael.kalygin@gmail.com>
2016-05-21 03:16:52 +0300

    cleanup jobs queue code

--- src/channel.build.web/server/jobs.js
+++ src/channel.build.web/server/jobs.js
@@ -1,15 +1,6 @@
-// TODO: Move workers code to a separate node.
-//       Docs: https://atmospherejs.com/vsivsi/job-collection.
 // TODO: Use more robust logging solution.
 var fs = Meteor.npmRequire('fs'),
     os = Meteor.npmRequire('os');
-
-// TODO: The issue is that workers fail to run because they have no access to the opened file...
-// var logsPath = `${process.env.HOME}/log/channel.build`;
-// shell.mkdir('-p', logsPath);
-// var chanJobsLogStream = fs.createWriteStream(`${logsPath}/chan.log`);
-// ChanJobs.setLogStream(chanJobsLogStream);
-// ChanJobs.setLogStream(process.stdout);
 
 ChanJobs.allow({
   admin: function (userId, method, params) {
@@ -18,7 +9,7 @@ ChanJobs.allow({
 });
 
 Meteor.startup(function () {
+  // Start Channel CLI jobs server for DDP connections.
+  // Connected workers will process incoming jobs.
   ChanJobs.startJobServer();
-  // Example:
-  // Job(ChanJobs, 'import', { channelId: 'cXYen6PdzBnoEPGqK' }).save();
 });
